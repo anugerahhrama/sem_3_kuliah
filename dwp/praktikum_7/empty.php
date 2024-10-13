751f44c8b08b619d0e939449e5279d0108acd6f9
anugerah rachmadani <anugerahrama@MacBook-Pro.local>
2024-10-13 23:53:09 +0700

    Praktikum 2: Function empty

---
+++ dwp/praktikum_7/empty.php
@@ -0,0 +1,13 @@
+<?php
+$myArray = array();
+if (empty($nama)) {
+    echo "Array tidak terdefinisi atau kosong. <br>";
+} else {
+    echo "Variabel terdefinisi dan tidak kosong";
+}
+
+if (empty($nonExistentVar)) {
+    echo "Variabel tidak terdefinisi atau kosong.";
+} else {
+    echo "Variabel terdefinisi dan tidak kosong";
+}
